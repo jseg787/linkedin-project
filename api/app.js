34402bf5d4fd4bd1b3eaee6847d4d5ddf14e7bd9
jseg787 <jsegarra3@gmail.com>
2021-03-16 15:54:38 -0400

    remove commented code from app.js

--- api/app.js
+++ api/app.js
@@ -7,29 +7,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(corse());
-// (async () => {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-// 	// await page.goto('https://www.google.com/search?q=linkedin');
-// 	await page.goto('https://example.com');
-// 	await page.screenshot({ path: 'example.png' });
-
-// 	await browser.close();
-// })();
-
-// (async () => {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-// 	await page.goto('https://example.com');
-// 	const hrefElement = await page.$('h1');
-// 	await hrefElement.screenshot({ path: 'h1.png' });
-// 	await browser.close();
-// })();
 
 async function getStuff(query) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	// await page.goto('https://www.google.com/search?q=https://www.linkedin.com/in/karellahmy/');
 	await page.goto(`https://www.google.com/search?q=${query}`);
 	const hrefElement = await page.$('.g');
 	const screenshot = await hrefElement.screenshot({
@@ -41,7 +22,6 @@ async function getStuff(query) {
 
 app.post('/search', async (req, res) => {
 	const { query } = req.body;
-	// console.log('got to the api');
 	const ss = await getStuff(query);
 	res.send(ss);
 });
